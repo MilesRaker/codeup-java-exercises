17113fa212cb43df66c9c77062e1424097ad3494
Miles Raker <milesraker@gmail.com>
2022-11-07 12:58:34 -0800

    Introduction to Java: Complete

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello,");
+        System.out.print(" World!");
+    }
+/*  renaming the "main" disables intelli-j's "run" function
+    public static void bananas(String[] args){
+        System.out.println("Hello, World!");
+    }*/
+}
+
+/* Running project in command line:
+* 1. cd src
+* 2. javac HelloWorld.java to compile
+* 3. java HelloWorld to run JVM*/
