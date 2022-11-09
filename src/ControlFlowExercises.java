bff49410c298f1adf51b8aea563ca7915522d21b
Miles Raker <milesraker@gmail.com>
2022-11-09 13:02:04 -0800

    Ex4 and Bonus complete, Grade converter

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,20 +2,78 @@ import java.util.Scanner;
 
 
 public class ControlFlowExercises {
-    public static void main(String[] args){
-        // Display a table of powers
-        Scanner sc = new Scanner(System.in);
-        System.out.println("USER! Enter an INTEGER!!");
-        int inputInt = sc.nextInt();
-        // display table of squares and cubes
-        System.out.println("Here is your table:");
-        System.out.println("number | squared | cubed");
-        System.out.println("------ | ------- | -----");
-        for(int i = 1; i <= inputInt; i++ ){
+    public static void main(String[] args) {
+/*        // convert numerical grade to alphabetic grade
+        boolean again = true;
 
-            long squared = i * i;
-            long cubed = i * i * i;
-            System.out.printf("%d      | %d       | %d    ", i, squared, cubed).println();
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a grade between 0 and 100: ");
+            int grade = sc.nextInt();
+            if (grade >= 88) {
+                System.out.println("Grade: A");
+            } else if (grade >= 80) {
+                System.out.println("Grade: B");
+            } else if (grade >= 67) {
+                System.out.println("Grade: C");
+            } else if (grade > 60) {
+                System.out.println("Grade: D");
+            } else {
+                System.out.println("Grade: F");
+            }
+
+            System.out.println("Would you like to continue? Enter a Y or N: ");
+            String userInputChar = sc.next();
+            if (userInputChar.equalsIgnoreCase("Y")) {
+                again = true;
+            } else {
+                again = false;
+            }
         }
+        while(again);*/
+
+        // Bonus: refactor to add pluses and minuses
+        // convert numerical grade to alphabetic grade
+        boolean again = true;
+
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a grade between 0 and 100: ");
+            int grade = sc.nextInt();
+            if (grade >= 93) {
+                System.out.println("Grade: A");
+            } else if (grade >= 90) {
+                System.out.println("Grade: A-");
+            } else if (grade >= 87) {
+                System.out.println("Grade: B+");
+            } else if (grade >= 83) {
+                System.out.println("Grade: B");
+            } else if (grade >= 80) {
+                System.out.println("Grade: B-");
+            } else if (grade >= 77) {
+                System.out.println("Grade: C+");
+            } else if (grade >= 73) {
+                System.out.println("Grade: C");
+            } else if (grade >= 70) {
+                System.out.println("Grade: C-");
+            } else if (grade >= 67) {
+                System.out.println("Grade: D+");
+            } else if (grade > 63) {
+                System.out.println("Grade: D");
+            } else if (grade >= 60) {
+                System.out.println("Grade: D-");
+            } else {
+                System.out.println("Grade: F");
+            }
+
+            System.out.println("Would you like to continue? Enter a Y or N: ");
+            String userInputChar = sc.next();
+            if (userInputChar.equalsIgnoreCase("Y")) {
+                again = true;
+            } else {
+                again = false;
+            }
+        }
+        while(again);
     }
 }
