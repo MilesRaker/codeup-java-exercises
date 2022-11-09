276137a74b99c1df7890907c27ceb91cd0f974aa
Miles Raker <milesraker@gmail.com>
2022-11-09 12:14:17 -0800

    Java Loops Exercise 1 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,35 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-        int i = 5;
-        while(i <= 15){
-            System.out.print(i + " ");
-            i++;
+/*        int i = 0;
+        do{
+            System.out.println(i);
+            i += 2;
+        } while(i<=100);*/
+
+/*        int i = 100;
+        do{
+            System.out.println(i);
+            i -= 5;
+        } while(i >= -10);*/
+
+/*        long i = 2; // needed to change from int to long
+        do{
+            System.out.println(i);
+            i = i * i;
+        } while(i <= 1000000);*/
+
+        // Refactoring to use for loops
+
+/*        for(int i = 0; i <= 100; i += 2){
+            System.out.println(i);
+        }*/
+
+/*        for(int i = 100; i>= -10; i -= 5){
+            System.out.println(i);
+        }*/
+
+        for(long i = 2; i <= 1000000; i = i * i){
+            System.out.println(i);
         }
     }
 }
