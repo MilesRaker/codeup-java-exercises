c1b15cc99271f097d1c9cfa4ff8c8c6db6d08dca
Miles Raker <milesraker@gmail.com>
2022-11-09 14:41:26 -0800

    Java Strings Ex 1 complete

---
+++ src/StringExercise.java
@@ -0,0 +1,20 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        // 1. String Basics
+        String education = "We don't need no education";
+        System.out.println("education = " + education);
+
+        String control = "We don't need no thought control";
+        System.out.println("control = " + control);
+
+        String checkIt = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println("checkIt = " + checkIt);
+
+        String drive = "In windows, the main drive is usually C:\\";
+        System.out.println("drive = " + drive);
+
+        String nextNonsense = "I can do backslashes\\, double backslahses \\\\,\n and the amazing triple backslash \\\\\\";
+        System.out.println("nextNonsense = " + nextNonsense);
+
+    }
+}
