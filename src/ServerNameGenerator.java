6aca72b8a4c0639af9b61871361e29dd052206c1
Miles Raker <milesraker@gmail.com>
2022-11-16 09:01:25 -0800

    Java Arrays ex 3 in progress

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ServerNameGenerator {
+public class ServerNameGenerator {
+    String[] adjectives = {"purple", "creative", "honest", "authentic", "joyful", "frightened",
+            "grieving", "desperate", "magnificent", "omnipotent", "funky"};
+    String[] nouns = {"humans", "kitten", "rocket", "banananas", "lamp", "music",
+            "love", "bacterium", "liar", "desperation"};
+
+    private static int getRandomNumber(int max){
+        return (int) (Math.random() * max);
+    }
+    public static String returnRandomElement(String[] arr){
+        int arrLength = arr.length;
+        int randIndex = getRandomNumber(arrLength);
+        return arr[randIndex];
+    }
+
+    public static void main(String[] args) {
+        ServerNameGenerator sng = new ServerNameGenerator();
+        String output = returnRandomElement(sng.adjectives) + "-" + returnRandomElement(sng.nouns);
+        System.out.println("Server Name: ");
+        System.out.println("output = " + output);
+    }
 }
